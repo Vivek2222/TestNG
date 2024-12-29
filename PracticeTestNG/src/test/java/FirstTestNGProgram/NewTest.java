87a5af7e08b377e3de8fa17b703c2eccd6f2f695
Vivek Patil <vivekpatil2291997@gmail.com>
2024-12-30 02:30:58 +0530

    Added TestNG Program

---
+++ PracticeTestNG/src/test/java/FirstTestNGProgram/NewTest.java
@@ -0,0 +1,15 @@
+package FirstTestNGProgram;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void firstTest() {
+	  System.out.println("This is a TestNG Program 1");
+  }
+  
+  @Test
+  public void secondTest() {
+	  System.out.println("This is a TestNG Program 2 ");
+  }
+}
