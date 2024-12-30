6765a5d89d1a9e68ea2b362fa178fca6c091f9e0
Vivek Patil <vivekpatil2291997@gmail.com>
2024-12-31 03:33:40 +0530

    Java Program

---
+++ PracticeTestNG/src/main/java/FirstTestNGProgram/JavaProgram.java
@@ -0,0 +1,10 @@
+package FirstTestNGProgram;
+
+public class JavaProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First JAVA Program using Main Method");
+	}
+
+}
