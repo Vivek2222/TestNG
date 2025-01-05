7ca3c4b7cc26c3b107968661904929749944b105
Vivek Patil <vivekpatil2291997@gmail.com>
2025-01-06 03:34:42 +0530

    Annotations

--- PracticeTestNG/src/test/java/annotations/HomePage.java
+++ PracticeTestNG/src/test/java/annotations/HomePage.java
@@ -13,50 +13,57 @@ import org.testng.annotations.AfterSuite;
 public class HomePage {
 	@Test
 	public void HomePageTestCase1() {
-		System.out.println("HomePage Method One");
+		System.out.println("HomePageTest Method One");
 	}
 	@Test
 	public void HomePageTestCase2() {
-		System.out.println("HomePage Method Two");
+		System.out.println("HomePageTest Method Two");
 	}
-
+	@Test
+	public void HomePageTestCase3() {
+		System.out.println("HomePageTest Method Three");
+	}
+	@Test
+	public void HomePageTestCase4() {
+		System.out.println("HomePageTest Method Four");
+	}
 	@BeforeMethod
 	public void beforeMethod() {
-		System.out.println("Before Method - HomePage");
+		System.out.println("Before Method - HomePageTest");
 	}
 
 	@AfterMethod
 	public void afterMethod() {
-		System.out.println("After Method - HomePage");
+		System.out.println("After Method - HomePageTest");
 	}
 
 	@BeforeClass
 	public void beforeClass() {
-		System.out.println("Before Class - HomePage");
+		System.out.println("Before Class - HomePageTest");
 	}
 
 	@AfterClass
 	public void afterClass() {
-		System.out.println("After Class - HomePage");
+		System.out.println("After Class - HomePageTest");
 	}
 
 	@BeforeTest
 	public void beforeTest() {
-		System.out.println("Before Test - HomePage");
+		System.out.println("Before Test - HomePageTest");
 	}
 
 	@AfterTest
 	public void afterTest() {
-		System.out.println("Before Class - HomePage");
+		System.out.println("Before Class - HomePageTest");
 	}
 
 	@BeforeSuite
 	public void beforeSuite() {
-		System.out.println("Before Suite - HomePage");
+		System.out.println("Before Suite - HomePageTest");
 	}
 
 	@AfterSuite
 	public void afterSuite() {
-		System.out.println("After Suite - HomePage");
+		System.out.println("After Suite - HomePageTest");
 	}
 }
