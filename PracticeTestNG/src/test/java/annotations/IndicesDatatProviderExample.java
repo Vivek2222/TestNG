a2559f1c017161b8bfec9669b35242c8f89107fc
Vivek Patil <vivekpatil2291997@gmail.com>
2025-01-15 12:36:58 +0530

    indices

---
+++ PracticeTestNG/src/test/java/annotations/IndicesDatatProviderExample.java
@@ -0,0 +1,24 @@
+package annotations;
+
+import org.testng.annotations.Test;
+
+public class IndicesDatatProviderExample {
+
+	@Test(dataProvider = "dataprovidermethod", dataProviderClass = IndicesDataProviderDemo.class)
+	public void loginTestcase(String username, String password) {
+
+		System.out.println("This is login testcase");
+		System.out.println("Username is: " + username);
+		System.out.println("Username is: " + password);
+	}
+
+	@Test(dataProvider = "dataprovidermethod", dataProviderClass = IndicesDataProviderDemo.class)
+	public void profileInfo(String name, String email, String address) {
+
+		System.out.println("This is Profile Info testcase");
+		System.out.println("Name is: " + name);
+		System.out.println("Email is: " + email);
+		System.out.println("Address is: " + address);
+	}
+
+}
