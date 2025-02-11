a2a3d443267b3ffbefb621c12ac6cbb1323a880a
Vivek Patil <vivekpatil2291997@gmail.com>
2025-02-11 20:59:51 +0530

    ignoremethodlevel

---
+++ PracticeTestNG/src/test/java/attributes_Part3/NewTestIgnoreMethodLevel.java
@@ -0,0 +1,32 @@
+package attributes_Part3;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Ignore;
+import org.testng.annotations.Test;
+import org.testng.annotations.AfterMethod;
+
+public class NewTestIgnoreMethodLevel {
+	
+	@Ignore
+	@Test
+	public void testMethod_1() {
+		System.out.println("This is Test Method 1");
+	}
+
+	@Test
+	public void testMethod_2() {
+		System.out.println("This is Test Method 2");
+	}
+
+	@BeforeMethod
+	public void beforeMethod() {
+		System.out.println("This is Before Method");
+	}
+
+	@AfterMethod
+	public void afterMethod() {
+		System.out.println("This is After Method");
+
+	}
+
+}
