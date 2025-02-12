57f5966ae235823ddf7d5a0a3355388859e6fc9a
Vivek Patil <vivekpatil2291997@gmail.com>
2025-02-12 12:23:40 +0530

    priority

---
+++ PracticeTestNG/src/test/java/attributes_Part1/PriorityExample.java
@@ -0,0 +1,37 @@
+package attributes_Part1;
+
+import org.testng.annotations.Test;
+
+public class PriorityExample {
+	
+	//Integer
+	//-2^31 to 2^31-1
+	//int minValue = Integer.MIN_VALUE; // -2147483648
+	//int maxValue = Integer.MAX_VALUE; // 2147483647
+	
+	@Test (priority=1)
+	public void loginPage() {
+		System.out.println("Login Page");
+	}
+	
+	@Test (priority=2)
+	public void productPage() {
+		System.out.println("Product Page");
+	}
+	
+	@Test (priority=3)
+	public void cartPage() {
+		System.out.println("Cart Page");
+	}
+	
+	@Test (priority=4)
+	public void checkoutPage() {
+		System.out.println("Checkout Page");
+	}
+	
+	@Test (priority=5)
+	public void paymentPage() {
+		System.out.println("Payment Page");
+	}
+
+}
