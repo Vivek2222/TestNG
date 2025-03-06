304e4a474a38299fe009dd25d2c54a9c2d6ad3bc
Vivek Patil <vivekpatil2291997@gmail.com>
2025-03-07 02:47:21 +0530

    optional parameter

---
+++ PracticeTestNG/src/test/java/parameters/OptionalParameters2.java
@@ -0,0 +1,17 @@
+package parameters;
+
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class OptionalParameters2 {
+
+	@Parameters({"username","password"})
+	@Test
+	public void loginTestcase(@Optional("User1") String username, @Optional("Pass1") String password) {
+
+		System.out.println("This is login testcase");
+		System.out.println("Username is: " + username);
+		System.out.println("Username is: " + password);
+	}
+	}
