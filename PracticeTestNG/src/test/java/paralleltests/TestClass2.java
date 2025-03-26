78ba051471ec446fc325f28e624deacd87e8f206
Vivek Patil <vivekpatil2291997@gmail.com>
2025-03-27 01:48:13 +0530

    parallel test

---
+++ PracticeTestNG/src/test/java/paralleltests/TestClass2.java
@@ -0,0 +1,30 @@
+package paralleltests;
+
+import org.testng.annotations.Test;
+
+public class TestClass2 {
+	
+	//Parallel - Thread: 5, 2to3 hrs
+	
+	//Sequential - Default Behavior - One by one
+	//1000 Tests - 10 hrs
+	
+	@Test
+	public void testMethod1() {
+		
+		System.out.println(" TestClass2 > TestMethod 1 : " +Thread.currentThread().threadId());
+	}
+	
+	@Test
+	public void testMethod2() {
+		
+		System.out.println(" TestClass2 > TestMethod 2 : "+Thread.currentThread().threadId());
+	}
+	
+	@Test
+	public void testMethod3() {
+		
+		System.out.println(" TestClass2 > TestMethod 3 : "+Thread.currentThread().threadId());
+	}
+
+}
